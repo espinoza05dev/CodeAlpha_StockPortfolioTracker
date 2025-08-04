71927342f398e8590b9fb6843e1c92efc264b830
Sbrentor <githubpersonal914@gmail.com>
2025-08-03 19:03:44 -0600

    .

---
+++ main.py
@@ -0,0 +1,15 @@
+# Dictionary to store stock prices (manually defined)
+stock_prices = {
+    "AAPL": 180,
+    "TSLA": 250,
+    "GOOGL": 140,
+    "MSFT": 350,
+    "AMZN": 145
+}
+
+# Dictionary to store user's portfolio
+portfolio = {}
+
+print("STOCK PORTFOLIO TRACKER")
+print("Available stocks:", list(stock_prices.keys()))
+print()
